9e918521957631a205af46e3002e20f02eb51009
wzbzdm <wzbzdm123@gmail.com>
2024-12-27 17:16:47 +0800

    添加Diamond图形

--- Diamond3D.cpp
+++ Diamond3D.cpp
@@ -4,3 +4,27 @@
 Diamond3D::Diamond3D(Graphics& gfx) {
 
 }
+
+void Diamond3D::SetSize(const DirectX::XMFLOAT3& size) {
+
+}
+
+void Diamond3D::Zoom(const DirectX::XMFLOAT3& scale) {
+
+}
+
+void Diamond3D::Update(float dt) noexcept {
+
+}
+
+DirectX::XMMATRIX Diamond3D::GetTransformMatrix() const noexcept {
+	return DirectX::XMMatrixTranslation(pos.x, pos.y, pos.z);
+}
+
+bool Diamond3D::RayIntersect(const Ray& ray, DirectX::XMFLOAT3& intersectionPoint) const noexcept {
+	return false;
+}
+
+void Diamond3D::InitColor() noexcept {
+
+}
